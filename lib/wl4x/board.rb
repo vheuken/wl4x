d16356303a3839497acf5bc3ded369402c73b47e
Vincent Heuken <me@vincentheuken.com>
2014-06-02 06:11:00 -0700

    cleaned up various functions in Board

--- lib/wl4x/board.rb
+++ lib/wl4x/board.rb
@@ -14,22 +14,22 @@ class Board
   end
   
   def get_hex(row, col)
-    if !valid_coordinates?(row, col) then
+    if !valid_coordinates?(row, col)
       raise ArgumentError.new("Invalid coordinates!")
     end
     
     @playing_pieces.uniq do |piece|
-      (piece.row == row) and (piece.col == col)
+      piece.row == row and piece.col == col
     end
   end
   
 private 
   def valid_coordinates?(row, col)
-    if (row.odd? and col == @width-1) then
+    if row.odd? and col == @width-1
       return false
-    elsif (row < 0) or (col < 0) then
+    elsif row < 0 or col < 0
       return false
-    elsif (row < @height) and (col < @width) then
+    elsif row < @height and col < @width
       return true
     end
     
@@ -39,8 +39,8 @@ private
   def initialize_system_markers
     @height-1.times do |row|
       @width-1.times do |col|
-        if !(row.odd? and col == @width-1) then
-          if get_hex(row, col).empty? then
+        if !(row.odd? and col == @width-1)
+          if get_hex(row, col).empty?
             @playing_pieces.push(SystemMarker.new(row, col))
           end
         end
